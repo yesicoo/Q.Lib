04b2bc59ea3cec8883d5dc5e9df8b65cd32093ba
xuqing <yesicoo@163.com>
2018-08-22 11:42:03 +0800

    修改方法

---
+++ Q.Lib/Utility/EncryptHelper.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q.Lib
+{
+    public class EncryptHelper
+    {
+        /// <summary>
+        /// 默认密钥
+        /// </summary>
+        public static readonly string _3DESKEY = "GgJjZhZz,YtSvJzHq.QingXu";
+
+        #region 3DES加密
+        ///  <summary > 
+        /// 3DES加密 
+        ///  </summary > 
+        ///  <param name="Value" >待加密字符串 </param > 
+        ///  <param name="sKey" >密钥 </param > 
+        ///  <returns >加密后字符串 </returns > 
+        public static string Encrypt3DES(string Value, string sKey)
+        {
+            string result = "";
+            //构造对称算法 
+            SymmetricAlgorithm mCSP = new TripleDESCryptoServiceProvider();
+
+            ICryptoTransform ct;
+            MemoryStream ms;
+            CryptoStream cs;
+            byte[] byt;
+            mCSP.Key = Encoding.UTF8.GetBytes(sKey);
+            //指定加密的运算模式 
+            mCSP.Mode = CipherMode.ECB;
+            //获取或设置加密算法的填充模式 
+            mCSP.Padding = PaddingMode.PKCS7;
+            ct = mCSP.CreateEncryptor(mCSP.Key, mCSP.IV);
+            byt = Encoding.UTF8.GetBytes(Value);
+            ms = new MemoryStream();
+            cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+            cs.Write(byt, 0, byt.Length);
+            cs.FlushFinalBlock();
+            cs.Close();
+
+            byte[] _result = ms.ToArray();
+            for (int i = 0; i < _result.Length; i++)
+            {
+                result += _result[i].ToString("X2").ToUpper();
+            }
+            return result;
+        }
+        #endregion
+
+        #region 3DES加密，采用默认密钥
+        /// <summary>
+        /// 3DES加密，采用默认密钥
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        public static string Encrypt3DES(string Value)
+        {
+            return Encrypt3DES(Value, _3DESKEY);
+        }
+        #endregion
+
+        #region 3DES解密
+        ///  <summary > 
+        /// 3DES解密 
+        ///  </summary > 
+        ///  <param name="Value" >待解密字符串(16进制)</param > 
+        ///  <param name="sKey" >密钥 </param > 
+        ///  <returns >解密后字符串</returns > 
+        public static string Decrypt3DES(string Value, string sKey)
+        {
+            //构造对称算法 
+            SymmetricAlgorithm mCSP = new TripleDESCryptoServiceProvider();
+
+            ICryptoTransform ct;
+            MemoryStream ms;
+            CryptoStream cs;
+            byte[] byt;
+            mCSP.Key = Encoding.UTF8.GetBytes(sKey);
+            mCSP.Mode = CipherMode.ECB;
+            mCSP.Padding = PaddingMode.PKCS7;
+            ct = mCSP.CreateDecryptor(mCSP.Key, mCSP.IV);
+
+            int len = Value.Length / 2;
+            byt = new byte[len];
+            for (int i = 0; i < len; i++)
+            {
+                byt[i] = Convert.ToByte(Value.Substring(i * 2, 2), 16);
+            }
+            ms = new MemoryStream();
+            cs = new CryptoStream(ms, ct, CryptoStreamMode.Write);
+            cs.Write(byt, 0, byt.Length);
+            cs.FlushFinalBlock();
+            cs.Close();
+            return Encoding.UTF8.GetString(ms.ToArray());
+
+
+        }
+        #endregion
+
+        #region 3DES解密，采用默认密钥
+        /// <summary>
+        /// 3DES解密，采用默认密钥
+        /// </summary>
+        /// <param name="Value"></param>
+        /// <returns></returns>
+        public static string Decrypt3DES(string Value)
+        {
+            if (!string.IsNullOrEmpty(Value))
+            {
+                return Decrypt3DES(Value, _3DESKEY);
+            }
+            else { return ""; }
+        }
+        #endregion
+    }
+}
---
+++ Q.Lib/Utility/QTools.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Q.Lib
+{
+    public static class QTools
+    {
+        #region 随机生成字符串
+        /// <summary>
+        /// 随机生成字符串
+        /// </summary>
+        /// <param name="num">位数</param>
+        /// <param name="type">
+        /// 0 -区分大小写字符包含数字的随机字符串
+        /// 1 -小写字符含数字字符串
+        /// 2 -大写字符包含数字字符串
+        /// 3 -小写字符随机字符串
+        /// 4 -大写字符随机字符串
+        /// 5 -仅数字
+        /// </param>
+        /// <returns></returns>
+        public static string RandomCode(int num, int type = 0)
+        {
+            string chars = null;
+
+            switch (type)
+            {
+                case 0:
+                    chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghicklnopqrstuvwxyz1234567890";
+                    break;
+                case 1:
+                case 2:
+                    chars = "abcdefghicklnopqrstuvwxyz1234567890";
+                    break;
+                case 3:
+                case 4:
+                    chars = "abcdefghicklnopqrstuvwxyz";
+                    break;
+                case 5:
+                    chars = "1234567890";
+                    break;
+                default:
+                    break;
+            }
+            if (!string.IsNullOrEmpty(chars))
+            {
+                int length = chars.Length;
+                StringBuilder sb = new StringBuilder();
+
+                for (int i = 0; i < num; i++)
+                {
+                    Random r = new Random(BitConverter.ToInt32(System.Guid.NewGuid().ToByteArray(), 0));
+                    sb.Append(chars[r.Next(0, length - 1)]);
+                }
+                var result = sb.ToString();
+                if (type == 2 || type == 4)
+                {
+                    result = result.ToUpper();
+                }
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        #endregion
+
+
+
+        #region 时间戳转换
+        /// <summary>  
+        /// 获取时间戳  
+        /// </summary>  
+        /// <returns></returns>  
+        public static long GetTimeStamp()
+        {
+            TimeSpan ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds);
+        }
+
+        /// <summary>
+        /// unix时间戳转换成日期
+        /// </summary>
+        /// <param name="unixTimeStamp">时间戳（秒）</param>
+        /// <returns></returns>
+        public static DateTime UnixTimestampToDateTime(this DateTime target, long timestamp)
+        {
+            var start = new DateTime(1970, 1, 1, 0, 0, 0);
+            return start.AddSeconds(timestamp).ToLocalTime();
+        }
+        #endregion
+
+        #region GUID字符串
+        /// <summary>
+        /// GUID字符串
+        /// </summary>
+        /// <returns></returns>
+        public static string GuidStr()
+        {
+            return Guid.NewGuid().ToString().Replace("-", "");
+        }
+        #endregion
+
+
+        #region 获取字符串MD5加密
+        /// <summary>
+        /// 获取字符串MD5加密
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        public static string GetMD5Hash(string input)
+        {
+            System.Security.Cryptography.MD5CryptoServiceProvider md5 = new System.Security.Cryptography.MD5CryptoServiceProvider();
+            byte[] bs = System.Text.Encoding.UTF8.GetBytes(input);
+            bs = md5.ComputeHash(bs);
+            System.Text.StringBuilder s = new System.Text.StringBuilder();
+            foreach (byte b in bs)
+            {
+                s.Append(b.ToString("x2").ToLower());
+            }
+            string password = s.ToString();
+            return password;
+        }
+        #endregion
+
+        #region GetMD5
+        /// <summary>
+        /// GetMD5
+        /// </summary>
+        /// <param name="encypStr"></param>
+        /// <param name="charset"></param>
+        /// <returns></returns>
+        public static string GetMD5(string encypStr, string charset)
+        {
+            string retStr;
+            MD5CryptoServiceProvider m5 = new MD5CryptoServiceProvider();
+
+            //创建md5对象
+            byte[] inputBye;
+            byte[] outputBye;
+
+            //使用GB2312编码方式把字符串转化为字节数组．
+            try
+            {
+
+                inputBye = Encoding.GetEncoding(charset).GetBytes(encypStr);
+            }
+            catch (Exception)
+            {
+                inputBye = Encoding.GetEncoding("GB2312").GetBytes(encypStr);
+            }
+            outputBye = m5.ComputeHash(inputBye);
+
+            retStr = System.BitConverter.ToString(outputBye);
+            retStr = retStr.Replace("-", "").ToUpper();
+            return retStr;
+        }
+        #endregion
+
+        /// <summary>
+        /// 加密
+        /// </summary>
+        /// <param name="unifiedorder"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string SignValue(SortedDictionary<string, string> unifiedorder, string key)
+        {
+            int i = 0;
+            string sign = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            foreach (KeyValuePair<string, string> temp in unifiedorder)
+            {
+                if (temp.Value == "" || temp.Value == null || temp.Key.ToLower() == "sign")
+                {
+                    continue;
+                }
+                i++;
+                sb.Append(temp.Key.Trim() + "=" + temp.Value.Trim() + "&");
+            }
+            sb.Append("key=" + key.Trim() + "");
+            string signkey = sb.ToString();
+            QLog.SendLog(signkey);
+            sign = GetMD5(signkey, "utf-8");
+
+            return sign;
+        }
+
+        /// <summary>
+        /// 把XML数据转换为SortedDictionary<string, string>集合
+        /// </summary>
+        /// <param name="strxml"></param>
+        /// <returns></returns>
+        public static SortedDictionary<string, string> GetInfoFromXml(string xmlstring)
+        {
+            SortedDictionary<string, string> sParams = new SortedDictionary<string, string>();
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xmlstring);
+                XmlElement root = doc.DocumentElement;
+                int len = root.ChildNodes.Count;
+                for (int i = 0; i < len; i++)
+                {
+                    string name = root.ChildNodes[i].Name;
+                    if (!sParams.ContainsKey(name))
+                    {
+                        sParams.Add(name.Trim(), root.ChildNodes[i].InnerText.Trim());
+                    }
+                }
+            }
+            catch { }
+            return sParams;
+        }
+        /// <summary>
+        /// 判断字符串空或者null
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        public static bool IsNullOrEmpty(params string[] args)
+        {
+            foreach (var item in args)
+            {
+                if (string.IsNullOrEmpty(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// 3DES 加密
+        /// </summary>
+        /// <returns></returns>
+        public static string E3DES(string value, string key = "")
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return EncryptHelper.Encrypt3DES(value);
+            }
+            else
+            {
+                return EncryptHelper.Encrypt3DES(value, key);
+            }
+        }
+
+        /// <summary>
+        /// 3DES 解密
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public static string D3DES(string value, string key = "")
+        {
+            if (string.IsNullOrEmpty(key))
+            {
+                return EncryptHelper.Decrypt3DES(value);
+            }
+            else
+            {
+                return EncryptHelper.Decrypt3DES(value, key);
+            }
+        }
+    }
+}
