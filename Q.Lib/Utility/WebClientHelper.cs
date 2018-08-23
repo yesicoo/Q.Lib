0afd50795fa4d7433e2e0fb3b756ba514ead1a6f
xuqing <yesicoo@163.com>
2018-08-23 18:33:17 +0800

    修改请求参数

--- Q.Lib/Utility/WebClientHelper.cs
+++ Q.Lib/Utility/WebClientHelper.cs
@@ -49,6 +49,7 @@ namespace Q.Lib
             {
                 try
                 {
+                    wc.Headers["Accept"] = "application/json, text/javascript, */*; q=0.01";
                     wc.Headers["Accept-Language"] = "zh-CN,zh;q=0.8,en;q=0.6,ja;q=0.4,zh-TW;q=0.2";
                     wc.Headers["Content-Type"] = "application/json";
                     wc.Headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36";
@@ -74,6 +75,7 @@ namespace Q.Lib
             }
             return t_result;
         }
+
 
         public static T Post<T>(string url, object parameter)
         {
