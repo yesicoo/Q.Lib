f858a328d0589fa99af2e559d0764f0e7df00f7f
XuQing <yesicoo@1139.com>
2018-11-16 01:05:01 +0800

    添加积乘方法

---
+++ Q.Lib/Extension/Integer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q.Lib.Extension
+{
+   public static class Integer
+    {
+        public static decimal Product(this IEnumerable<decimal> muns)
+        {
+            decimal result = 0.00m;
+            var l_nums = muns.ToList();
+            for (int i = 0; i < l_nums.Count; i++)
+            {
+                if (i == 0)
+                {
+                    result = l_nums[i];
+                }
+                else
+                {
+                    result = result * l_nums[i];
+                }
+            }
+            return result;
+        }
+    }
+}
