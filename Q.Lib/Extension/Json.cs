e792b9ef9823fb94c6e7b7093dd0320338900f23
xuqing <yesicoo@163.com>
2019-04-19 18:07:40 +0800

    优化json转换

---
+++ Q.Lib/Extension/Json.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q.Lib.Extension
+{
+    public static class Json
+    {
+        public static string ToJsonStr(this object o)
+        {
+            if (o == default(object))
+            {
+                return null;
+            }
+            else
+            {
+                return JsonConvert.SerializeObject(o);
+            }
+        }
+
+        public static T ToObj<T>(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return default(T);
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<T>(str);
+            }
+        }
+    }
+}
