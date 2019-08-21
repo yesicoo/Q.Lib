7f714de6e9d3538d3204e9f1e79202cfaa23d807
xuqing <code@xuqing.me>
2019-08-21 12:14:42 +0800

    添加IDictionary扩展方法

---
+++ Q.Lib/Extension/IDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q.Lib.Extension
+{
+    public static class IDictionary
+    {
+        public static T GetValue<T>(this IDictionary<object, object> dic, object key)
+        {
+            if (dic.ContainsKey(key))
+            {
+                return (T)dic[key];
+            }
+            else
+            {
+                return default(T);
+            }
+        }
+
+        public static bool TryGetValue<T>(this IDictionary<object, object> dic, object key, out T t)
+        {
+            if (dic.ContainsKey(key))
+            {
+                t = (T)dic[key];
+                return true;
+            }
+            else
+            {
+                t = default(T);
+                return false;
+            }
+        }
+    }
+}
