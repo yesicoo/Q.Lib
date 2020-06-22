85bd40458a02946df220aa89a9507fca53ba39b4
Qing <code@xuqing.me>
2020-06-22 17:11:40 +0800

    添加DateTime.ToString()扩展方法

---
+++ Q.Lib/Extension/DateTime_.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q.Lib.Extension
+{
+   public static class DateTime_
+    {
+        public static string ToString(this DateTime dateTime)
+        {
+            return dateTime.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
