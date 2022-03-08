fb4962b0c51a17ea078caf3baffa28eeb712cf42
Qing <code@xuqing.me>
2022-03-08 09:28:24 +0800

    优化随机字符串生成

--- Q.Lib/Utility/QTools.cs
+++ Q.Lib/Utility/QTools.cs
@@ -99,12 +99,34 @@ namespace Q.Lib
 
         #region GUID字符串
         /// <summary>
-        /// GUID字符串
+        /// GUID字符串 32位
         /// </summary>
         /// <returns></returns>
         public static string GuidStr()
         {
             return Guid.NewGuid().ToString().Replace("-", "");
+        }
+        /// <summary>
+        /// 16位字符串
+        /// </summary>
+        /// <returns></returns>
+        public static string GuidShortStr()
+        {
+            long i = 1;
+            foreach (byte b in Guid.NewGuid().ToByteArray())
+            {
+                i *= ((int)b + 1);
+            }
+            return string.Format("{0:x}", i - DateTime.Now.Ticks);
+        }
+        /// <summary>
+        /// 19位数字
+        /// </summary>
+        /// <returns></returns>
+        public static long GuidNum()
+        {
+            byte[] buffer = Guid.NewGuid().ToByteArray();
+            return BitConverter.ToInt64(buffer, 0);
         }
         #endregion
 
