ab1b95c67c1bd8d7de92cb1dd0efb759c9047859
Qing <code@xuqing.me>
2023-03-14 17:19:01 +0800

    添加Q.DI

---
+++ Q.Lib/Utility/QDI.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Q.Lib.Utility
+{
+
+    public class QDI
+    {
+        public static IServiceCollection Services { get; set; }
+        public static IServiceProvider ServiceProvider { get; set; }
+    }
+
+    public static class QDIExtensions
+    {
+        public static IServiceCollection AddQingDI(this IServiceCollection services)
+        {
+            QDI.Services = services;
+            return services;
+        }
+
+        public static IApplicationBuilder UseQingDI(this IApplicationBuilder builder)
+        {
+            QDI.ServiceProvider = builder.ApplicationServices;
+            return builder;
+        }
+    }
+}
